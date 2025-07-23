6f2573843ac56db6b6d00e1081cf2307f9663c39
Samariddinn <urinovsalohiddin077@gmail.com>
2025-07-23 11:15:34 +0500

    Added partner section

--- src/app/datas/partners.data.ts
+++ src/app/datas/partners.data.ts
@@ -3,47 +3,47 @@ import { PartnersModel } from '../models/partners.model';
 export const partnersData: PartnersModel[] = [
   {
     id: 1,
-    img: 'images/partners/partner-1.webp',
-    text: 'JABATAN AGAMA ISLAM WILAYAH PERSEKUTUAN',
+    img: 'images/partners/real-partner.webp',
+    text: 'INSTITUTE FATWA DAN HALAL',
   },
 
-  {
-    id: 2,
-    img: 'images/partners/partner-2.webp',
-    text: 'USIM',
-  },
+  // {
+  //   id: 2,
+  //   img: 'images/partners/partner-2.webp',
+  //   text: 'USIM',
+  // },
 
-  {
-    id: 3,
-    img: 'images/partners/partner-3.webp',
-    text: 'AMANIE ADVISORS',
-  },
+  // {
+  //   id: 3,
+  //   img: 'images/partners/partner-3.webp',
+  //   text: 'AMANIE ADVISORS',
+  // },
 
-  {
-    id: 4,
-    img: 'images/partners/partner-4.webp',
-    text: 'ANGKASA',
-  },
-  {
-    id: 5,
-    img: 'images/partners/partner-5.webp',
-    text: 'SMIAAG',
-  },
-  {
-    id: 6,
-    img: 'images/partners/partner-6.webp',
-    text: 'BANK MUAMALAT',
-  },
+  // {
+  //   id: 4,
+  //   img: 'images/partners/partner-4.webp',
+  //   text: 'ANGKASA',
+  // },
+  // {
+  //   id: 5,
+  //   img: 'images/partners/partner-5.webp',
+  //   text: 'SMIAAG',
+  // },
+  // {
+  //   id: 6,
+  //   img: 'images/partners/partner-6.webp',
+  //   text: 'BANK MUAMALAT',
+  // },
 
-  {
-    id: 7,
-    img: 'images/partners/partner-7.webp',
-    text: 'FELDA',
-  },
+  // {
+  //   id: 7,
+  //   img: 'images/partners/partner-7.webp',
+  //   text: 'FELDA',
+  // },
 
-  {
-    id: 8,
-    img: 'images/partners/partner-8.webp',
-    text: 'KOPERASI PETRONAS',
-  },
+  // {
+  //   id: 8,
+  //   img: 'images/partners/partner-8.webp',
+  //   text: 'KOPERASI PETRONAS',
+  // },
 ];
